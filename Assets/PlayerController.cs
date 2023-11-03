eb7600d015bfd2a627b2d29ec6f1b322361053f7
Hypno <146860564+Hypnoticsuri@users.noreply.github.com>
2023-11-03 10:28:59 -0700

    Almost done with 4.3

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     private GameObject focalPoint;
     public bool hasPowerup = false;
+    private float powerupStrength = 15.0f;
+    public GameObject powerupIndicator;
 
 
 
@@ -24,6 +26,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
+      
+
+
+
 
         float fowardInput = Input.GetAxis("Vertical");
         
@@ -34,21 +40,37 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    IEnumerator PowerupCountdownRoutine ()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true); 
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
         }
-       //private void OnCollisionEnter(Collision collision)
+    
+      
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
-
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            
+            Debug.Log("Player collided with" + collision.gameObject.name + "with powerup set to" + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
         }
-
-
-
-
     }
 
 
